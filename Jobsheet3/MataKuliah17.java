627e0e40520a838eadb240a51bc5408bc87b93e9
lindwnrl <lindwnrl@gmail.com>
2026-02-24 19:47:28 +0700

    Percobaan Final

---
+++ Jobsheet3/MataKuliah17.java
@@ -0,0 +1,27 @@
+package Jobsheet3;
+
+public class MataKuliah17 {
+    public String kode;
+    public String nama;
+    public int sks;
+    public int jumlahJam;
+    
+    public MataKuliah17(String kode,String nama,int sks,int jumlahJam){
+        this.kode =kode;
+        this.nama = nama;
+        this.sks = sks;
+        this.jumlahJam = jumlahJam;
+    }
+    void tambahData(String kode,String nama,int sks,int jumlahJam){
+        this.kode =kode;
+        this.nama = nama;
+        this.sks = sks;
+        this.jumlahJam = jumlahJam;
+    }
+    void cetakInfo(){
+        System.out.println("Kode       :"+kode);
+        System.out.println("Nama       :"+nama);
+        System.out.println("SKS        :"+sks);
+        System.out.println("Jumlah Jam :"+jumlahJam);
+    }
+}
