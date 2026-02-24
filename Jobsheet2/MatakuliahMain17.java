0088cc0a0f27b6b2b1564bef4c17917113cc79f0
lindwnrl <lindwnrl@gmail.com>
2026-02-24 19:46:54 +0700

    Percobaan Final

---
+++ Jobsheet2/MatakuliahMain17.java
@@ -0,0 +1,23 @@
+package Jobsheet2;
+
+public class MatakuliahMain17 {
+    public static void main(String[] args) {
+        MataKuliah17 mk1 =new MataKuliah17(null, null, 0, 0);
+        mk1.nama = "El Pollo";
+        mk1.kodeMK = "AL";
+        mk1.sks = 4;
+        mk1.jumlahJam = 3;
+
+        mk1.tampilkanInformasi();
+        System.out.println();
+        mk1.kurangJam(2);
+        mk1.ubahSks(1);
+        mk1.tampilkanInformasi();
+        System.out.println();
+
+        MataKuliah17 mk2 = new MataKuliah17("Los Bijos", "Basis Data", 3, 4);
+        mk2.ubahSks(2);
+        mk2.kurangJam(2);
+        mk2.tampilkanInformasi();
+    }
+}
