4d628c97c513eb2f951f43b3e019fb9c7c6164e1
lindwnrl <lindwnrl@gmail.com>
2026-03-01 14:09:44 +0700

    Praktikum1

--- Jobsheet2/MataKuliah17.java
+++ Jobsheet2/MataKuliah17.java
@@ -11,13 +11,20 @@ public class MataKuliah17{
         kodeMK = koMK;
         this.sks = sks;
         jumlahJam =jmlJam;
+        
     }
 
 void tampilkanInformasi(){
     System.out.println("Nama : " + nama);
     System.out.println("Kode MK : " + kodeMK);
     System.out.println("SKS : " + sks);
-    System.out.println("Jumlah Jam : "+ jumlahJam);
+    if (jumlahJam <= 0) {
+        System.out.println("Pengurangan gagal : harap memasukkan jumlah yang lebih besar dari nol");
+    }
+    else {
+        System.out.println("Jumlah Jam : "+ jumlahJam);
+
+    }
 }
 void ubahSks(int sksBaru){
 sks = sksBaru;
@@ -26,9 +33,7 @@ void tambahJam(int jam){
 jumlahJam += jam;
 }
 void kurangJam(int jam){
-jumlahJam -= jam;
-if (jam <= 0) {
-    
+    jumlahJam -= jam;
 }
 }
-}
+
